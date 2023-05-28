1f2346ba8d6c2c36d209efce0730d623cc84930c
GroveDG <87248833+GroveDG@users.noreply.github.com>
2023-05-28 19:46:57 -0400

    JavaScript Intro Assessment

---
+++ JavaScript Intro Assessment/script.js
@@ -0,0 +1,7 @@
+var firstName = "Grove";
+var lastName = "DeGroof";
+var age = 19;
+
+alert("My name is " + firstName + " " + lastName);
+alert("I am " + age + " years old");
+console.log("Definitely more fun than most.")
